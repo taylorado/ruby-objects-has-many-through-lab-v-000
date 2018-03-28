ebdc0ba9bfcb392ac428d687f402c1464fe9ddda
Taylor Donlan <taylordonlan@gmail.com>
2018-03-28 16:02:20 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+class Genre
+  attr_accessor :name
+  attr_reader :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def artists
+    @songs.collect { |song| song.artist }
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+end
